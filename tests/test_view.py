3e14d1efef1b832291bca6f5b4ce394c431b613b
Fabian <fabian.witt@t-online.de>
2020-07-09 13:13:08 +0200

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.test import Client
 
 from tapp.models import Foo
