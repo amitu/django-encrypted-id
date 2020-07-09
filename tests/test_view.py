960053116afc5753a210e7995fb306c0f5f4a294
Fabian <fabian.witt@t-online.de>
2020-07-09 13:19:09 +0200

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -16,7 +16,7 @@ def test_view(db):
     client = Client()
 
     foo = Foo.objects.create(text="hello")
-    url = reverse("foo", args=(foo.ekey,))
+    url = reverse("foo", kwargs={'slug': foo.ekey})
     response = client.get(url)
     response_object = response.context["object"]
     assert response_object == foo
